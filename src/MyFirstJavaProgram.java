fa4f81edf635884fd0b5b1dd83a3b4d0053b4a5a
Mukesh Kumar <singh88rajat@gmail.com>
2016-12-17 21:15:04 +0530

    CoreJava1stAassignment

---
+++ src/MyFirstJavaProgram.java
@@ -0,0 +1,19 @@
+
+public class MyFirstJavaProgram {
+    
+	  public static void main(String args[]){
+	    	int x=10; 
+	    	int y=20;   	
+	    	System.out.println("Numbers Before Swap:");
+	    	System.out.println("X Value:" +x);
+	    	System.out.println("Y Value:" +y);
+	    	x=x+y;
+	    	y=x-y;
+	    	x=x-y;
+	        System.out.println("Numbers After Swap:");
+	    	System.out.println("X Value:" +x);
+	    	System.out.println("Y Value:" +y);  
+	    	
+	    	
+}
+}
